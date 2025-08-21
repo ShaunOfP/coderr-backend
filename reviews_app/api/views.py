ef3c781c803a08771686ffba9da5b5429a28aa82
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-08-21 12:35:32 +0200

    Added ordering, filtering, documentation

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from reviews_app.models import Review
 from .serializers import ReviewSerializer
@@ -7,8 +9,17 @@ from .permissions import IsCustomer, IsReviewCreator, HasNoReviewForThisBusiness
 
 
 class ReviewListCreateView(ListCreateAPIView):
+    """
+    This view allows the user to use the GET and POST-method.
+    Filtering for business_user_id and reviewer_id are supported.
+    Also supports ordering for updated_at and rating.
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['business_user_id', 'reviewer_id']
+    ordering_fields = ['updated_at', 'rating']
+    ordering = ['updated_at', 'rating']
 
     def get_permissions(self):
         """
@@ -21,6 +32,7 @@ class ReviewListCreateView(ListCreateAPIView):
 
 
 class ReviewUpdateDeleteView(DestroyAPIView, UpdateAPIView):
+    """This view allows for DELETE and PATCH/PUT calls"""
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsReviewCreator]
